61de5f8b3682c7069955823856b113d022167140
mariiabogdanova <februarymary@gmail.com>
2018-02-01 19:27:27 +0200

    Added www

---
+++ www/js/index.js
@@ -0,0 +1,48 @@
+document.addEventListener("deviceready", onDevice, false);
+
+function onDevice() {
+document.getElementById("btn1").addEventListener("click", takePicture);
+document.getElementById("btn2").addEventListener("click", openFilePicker);
+
+function takePicture() {
+  navigator.camera.getPicture(onSuccess,onError,{
+    quality:50,
+    destinationType:Camera.DestinationType.DATA_URL
+});
+}
+
+function onSuccess(imageData) {
+  var img=document.getElementById("img1");
+  img.src="data:image/jpeg; base64,"+imageData;
+
+
+ }
+
+ function onError(err) {
+  // console.log
+  alert("erorrbhgy ----- >" + err);
+ }
+
+
+
+function openFilePicker(){
+
+
+    navigator.camera.getPicture(function(result){
+        console.log(result);
+        var img=document.getElementById("img1");
+        img.src="data:image/jpeg; base64,"+result;
+    },
+
+      function(error){
+        console.log(error);
+        },
+  {
+sourceType : Camera.PictureSourceType.SAVEDPHOTOALBUM
+});
+
+
+
+}
+
+}
